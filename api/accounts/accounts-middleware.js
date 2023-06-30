c2cf81188f7de5814837538d2bbebc74b635ece0
egekulahli <egekulahli@gmail.com>
2023-06-30 03:26:42 +0300

    initial changes

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,12 +1,59 @@
 exports.checkAccountPayload = (req, res, next) => {
   // KODLAR BURAYA
   // Not: Validasyon için Yup(şu an yüklü değil!) kullanabilirsiniz veya kendiniz manuel yazabilirsiniz.
+  try {
+    let {name, budget} = req.body;
+    if (!name || !budget) {
+      res.status(400).json({message: "name and budget are required"});
+    }  else if (typeof name !== 'string') {
+      res.status(400).json({message: "name of account must be a string"});
+    }
+    else if (name.trim().length < 3 || name.trim().length > 100) {
+      res.status(400).json({message: "name of account must be between 3 and 100"});
+    }
+    else if (typeof budget !== 'number') {
+      res.status(400).json({message: "budget of account must be a number"});
+    }
+    else if (budget < 0 || budget > 1000000) {
+      res.status(400).json({message: "budget of account is too large or too small"});
+    }
+    else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // KODLAR BURAYA
+  try {
+    const {name} = req.body;
+    const existing = db('accounts').where('name', name.trim()).first();
+    if (existing) {
+      res.status(400).json({message: "that name is taken"});
+    }
+    else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 exports.checkAccountId = (req, res, next) => {
   // KODLAR BURAYA
+  try {
+    const {id} = req.params;
+    const existing = db('accounts').where('id', id).first();
+    if (!existing) {
+      res.status(404).json({message: "account not found"});
+    }
+    else {
+      req.account = existing;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
--- queries.sql
+++ queries.sql
@@ -1,17 +1,23 @@
 -- Veritabanı Sorguları
 
 -- Posta kodu 1010 olan tüm müşterileri bulun
-
+SELECT * FROM [Customers] where PostalCode = '1010'
 -- id'si 11 olan tedarikçinin telefon numarasını bulun
-
+SELECT Phone FROM [Suppliers] where SupplierID = '11'
 -- Verilen ilk 10 siparişi, sipariş tarihine göre azalan şekilde listeleyin
-
+SELECT * FROM [Orders] 
+order by OrderDate desc
+limit 10
 -- Londra, Madrid veya Brezilya'da yaşayan tüm müşterileri bulun
-
+SELECT * FROM [Customers] 
+where Country = 'Brazil' or City in ('London','Madrid')
 -- "The Shire" için bir müşteri kaydı ekleyin, ilgili kişi adı "Bilbo Baggins", adres - "Bag End" içinde "1 Hobbit-Hole", posta kodu "111" ve ülke "Middle Earth"
-
+insert into Customers 
+(CustomerID, CustomerName,ContactName, Address, City, PostalCode, Country)
+values ('117','The Shire', 'Bilbo Baggins', '1 Hobbit-Hole','The Shire', '111', 'Middle Earth')
 -- Posta kodu "11122" olarak değişecek şekilde Bilbo Baggins kaydını güncelleyin
-
+Update [Customers] set PostalCode = '11122', City = 'Istanbul' where ContactName = 'Bilbo Baggins'
 -- (Zorlayıcı Görev) Müşteriler tablosunda kaç farklı şehrin saklandığını keşfetmek için bir sorgu bulun. Tekrarlar çift sayılmamalıdır
-
+SELECT Count(DISTINCT City) from Customers
 -- (Zorlayıcı Görev) 20 karakterden uzun adları olan tüm tedarikçileri bulun. Adın uzunluğunu almak için "length(SupplierName)" kullanabilirsiniz.
+SELECT * from Suppliers where length(SupplierName) > 20
